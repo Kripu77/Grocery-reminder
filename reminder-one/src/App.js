eee23fa5bced22394b0703149b2dc40585e2c92c
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-18 19:24:50 +1100

    Components addition

--- reminder-one/src/App.js
+++ reminder-one/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './Components/Header'
 import "./CSS/Style.css"
 const App = () => {
     return (
         <div>
-        test
+       <Header/>
         </div>
     )
 }
